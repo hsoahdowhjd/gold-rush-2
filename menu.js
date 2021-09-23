caaa61792b3ea55ded417b68ad3a9ff7fa508a45
hsoahdowhjd <85369936+hsoahdowhjd@users.noreply.github.com>
2021-09-23 19:50:00 -0400

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,10 @@
+class Menu{
+     constructor(){
+      this.bg = createElement("body")
+      this.bg.style("backgroundcolor", "#C99A19")
+      this.start = createButton("start")
+      this.start.position(500, 450)
+      this.start.mousePressed()
+     }
+     
+}
